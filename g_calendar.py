83889bcfa53af65f13a575fbd7df44d0b3a1137a
ARYA CHAKRABORTY <aryachakraborty2002@gmail.com>
2025-01-21 20:49:42 +0530

    changed g_calendar.py

--- g_calendar.py
+++ g_calendar.py
@@ -1,31 +1,31 @@
-from phi.agent import Agent
-from phi.model.google import Gemini
-from phi.tools.googlecalendar import GoogleCalendarTools
-import datetime
-import os
-from tzlocal import get_localzone_name
-import google.generativeai as genai
+# from phi.agent import Agent
+# from phi.model.google import Gemini
+# from phi.tools.googlecalendar import GoogleCalendarTools
+# import datetime
+# import os
+# from tzlocal import get_localzone_name
+# import google.generativeai as genai
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
-API_KEY=os.getenv("GOOGLE_API_KEY")
-if API_KEY:
-    genai.configure(api_key=API_KEY)
+# API_KEY=os.getenv("GOOGLE_API_KEY")
+# if API_KEY:
+#     genai.configure(api_key=API_KEY)
 
-agent = Agent(
-    model=Gemini(id="gemini-2.0-flash-exp"),
-    tools=[GoogleCalendarTools(credentials_path="google_credentials.json")],
-    show_tool_calls=True,
-    instructions=[
-        f"""
-        You are scheduling assistant . Today is {datetime.datetime.now()} and the users timezone is {get_localzone_name()}.
-        You should help users with answers from their queries and from their Google calendar.
-        """
-    ],
-    add_datetime_to_instructions=True,
-)
+# agent = Agent(
+#     model=Gemini(id="gemini-2.0-flash-exp"),
+#     tools=[GoogleCalendarTools(credentials_path="google_credentials.json")],
+#     show_tool_calls=True,
+#     instructions=[
+#         f"""
+#         You are scheduling assistant . Today is {datetime.datetime.now()} and the users timezone is {get_localzone_name()}.
+#         You should help users with answers from their queries and from their Google calendar.
+#         """
+#     ],
+#     add_datetime_to_instructions=True,
+# )
 
-agent.print_response("Give me the list of tomorrow's events", markdown=True)
+# agent.print_response("Give me the list of tomorrow's events", markdown=True)
 
 
